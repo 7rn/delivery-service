91e2801cdd883716e5cfb21c6b0efb4b66d3b4c9
anonymoussc <50c5ac69@opayq.com>
2018-03-28 21:03:44 +0700

    Dummy-login and logout: named routes

--- api/User/Routes/web.php
+++ api/User/Routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
 Route::group(['middleware' => 'web', 'prefix' => 'user', 'namespace' => 'Api\User\Http\Controllers'], function() {
-    //
+    Route::get('/dummy-login', 'UserController@welcome')->name('login');
+    Route::get('/dummy-register', 'UserController@welcome')->name('register');
 });
